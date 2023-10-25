6cbfbf09c0365b61550a15e3a400877435f102df
joaovitordev <jv17fc@gmail.com>
2023-10-24 22:19:27 -0300

    criando post users

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Express from "express";
+import UserController from "./controllers/UserController";
 
 const app = Express();
 app.use(Express.json());
@@ -6,7 +7,9 @@ const PORT = 8000;
 
 app.get('/', (request, response) => {
     return response.send({ message: 'Hello World' })
-})
+});
+
+app.post('/createUser', UserController.createUser);
 
 app.listen(PORT, () => {
     console.log(`Server is running ${PORT}`);
